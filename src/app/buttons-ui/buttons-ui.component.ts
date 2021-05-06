fd756153e12f51ea22f858c8503a7d84afad59ed
Shubham <Shubham.BHardwaj@Xduce.com>
2021-05-06 19:32:23 +0530

    Css Changes

--- src/app/buttons-ui/buttons-ui.component.ts
+++ src/app/buttons-ui/buttons-ui.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-buttons-ui',
   templateUrl: './buttons-ui.component.html',
-  styleUrls: ['./buttons-ui.component.css']
+  styleUrls: ['./buttons-ui.component.scss']
 })
 export class ButtonsUiComponent implements OnInit {
 
