1f3ae75a8a26cae19d1ce5ba032a111d45a5e5d5
Shubham <Shubham.BHardwaj@Xduce.com>
2021-05-07 18:18:04 +0530

    Comma separated multiple email validation

--- src/app/input-ui/input-ui.component.ts
+++ src/app/input-ui/input-ui.component.ts
@@ -12,6 +12,10 @@ export class InputUiComponent implements OnInit {
   EmailPattern = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$';
   PhonePattern = '^[0-9]{10}$';
   PhonePattern1 = '^[0-9]{9}$'
+  PhonePattern13 = '^[0-9]{13}$'
+  PhonePattern11 = '^[0-9]{11}$'
+  PhonePattern12 = '^[0-9]{12}$'
+  PhonePattern15 = '^[0-9]{15}$'
   selectedNode: string;
   selectedNode1: string;
   selectedNode2: string;
@@ -31,7 +35,12 @@ export class InputUiComponent implements OnInit {
       {countryCode: '+1', country: './../../assets/Usa-img.png'},
       {countryCode: '+1', country: './../../assets/Canada-img.png'},
       {countryCode: '+44', country: './../../assets/Uk-img.png'},
-      {countryCode: '+31', country: './../../assets/Netherland-img.png'}
+      {countryCode: '+31', country: './../../assets/Netherland-img.png'},
+      {countryCode: '+49', country: './../../assets/Germany-img.png'},
+      {countryCode: '+39', country: './../../assets/Italy-img.png'},
+      {countryCode: '+81', country: './../../assets/Japan-img.png'},
+      {countryCode: '+86', country: './../../assets/China-img.png'},
+      {countryCode: '+61', country: './../../assets/Australia-img.png'}
     ];
     this.fieldDisable = true;
   }
@@ -51,52 +60,45 @@ export class InputUiComponent implements OnInit {
     return regexp.test(event);
   }
 
+  testPhoneNumber13(event) {
+    let regexp = new RegExp('^[0-9]{13}$');
+    return regexp.test(event);
+  }
+
+  testPhoneNumber11(event) {
+    let regexp = new RegExp('^[0-9]{11}$');
+    return regexp.test(event);
+  }
+
+  testPhoneNumber12(event) {
+    let regexp = new RegExp('^[0-9]{12}$');
+    return regexp.test(event);
+  }
+
+  testPhoneNumber15(event) {
+    let regexp = new RegExp('^[0-9]{15}$');
+    return regexp.test(event);
+  }
+
   EmailValidate(event) {
-    if (!this.testEmail(event)) {
-      this.emailValidation = true;
-      this.EmailMsg = 'Email is Invalid !';
-    }
-
-    if (this.testEmail(event)) {
-      this.emailValidation = false;
-    }
-
-    if (event == '') {
-      this.emailValidation = true;
-      this.EmailMsg = 'Email Input is Empty !';
-    }
-  }
-
-  PhoneValidate(event) {
-    if (!this.testPhoneNumber(event)) {
-      this.phoneValidation = true;
-      this.PhoneMsg = 'Phone Number is Invalid !';
-    }
-
-    if (this.testPhoneNumber(event)) {
-      this.phoneValidation = false;
-    }
-
-    if (event == '') {
-      this.phoneValidation = true;
-      this.PhoneMsg = 'Phone Number Input is Empty !';
-    }
-  }
-
-  PhoneValidate1(event) {
-    if (!this.testPhoneNumber1(event)) {
-      this.phoneValidation1 = true;
-      this.PhoneMsg1 = 'Phone Number is Invalid !';
-    }
-
-    if (this.testPhoneNumber1(event)) {
-      this.phoneValidation1 = false;
-    }
-
-    if (event == '') {
-      this.phoneValidation1 = true;
-      this.PhoneMsg1 = 'Phone Number Input is Empty !';
-    }
+    let separatedValues = event.replace(/\s/g,'').split(",");
+ 
+    separatedValues.map( e => {
+      if (!this.testEmail(e)) {
+        this.emailValidation = true;
+        this.EmailMsg = 'Email is Invalid !';
+      }
+  
+      if (this.testEmail(e)) {
+        this.emailValidation = false;
+      }
+  
+      if (e == '') {
+        this.emailValidation = true;
+        this.EmailMsg = 'Email Input is Empty !';
+      }
+    })
+    
   }
 
   getCountry(value) {
@@ -131,6 +133,77 @@ export class InputUiComponent implements OnInit {
         }
     
         if (this.testPhoneNumber1(event)) {
+          this.phoneValidation = false;
+        }
+    
+        if (event == '') {
+          this.phoneValidation = true;
+          this.PhoneMsg = 'Phone Number Input is Empty !';
+        }
+      }
+
+
+      if((res['countryCode'] === '+49') || (res['countryCode'] === '+81')) {
+        this.PhonePatternValid = this.PhonePattern13;
+        if (!this.testPhoneNumber13(event)) {
+          this.phoneValidation = true;
+          this.PhoneMsg = 'Phone Number is Invalid !';
+        }
+    
+        if (this.testPhoneNumber13(event)) {
+          this.phoneValidation = false;
+        }
+    
+        if (event == '') {
+          this.phoneValidation = true;
+          this.PhoneMsg = 'Phone Number Input is Empty !';
+        }
+      }
+
+      if((res['countryCode'] === '+39')) {
+        this.PhonePatternValid = this.PhonePattern11;
+        if (!this.testPhoneNumber11(event)) {
+          this.phoneValidation = true;
+          this.PhoneMsg = 'Phone Number is Invalid !';
+        }
+    
+        if (this.testPhoneNumber11(event)) {
+          this.phoneValidation = false;
+        }
+    
+        if (event == '') {
+          this.phoneValidation = true;
+          this.PhoneMsg = 'Phone Number Input is Empty !';
+        }
+      }
+
+
+      if((res['countryCode'] === '+86')) {
+        this.PhonePatternValid = this.PhonePattern12;
+        if (!this.testPhoneNumber12(event)) {
+          this.phoneValidation = true;
+          this.PhoneMsg = 'Phone Number is Invalid !';
+        }
+    
+        if (this.testPhoneNumber12(event)) {
+          this.phoneValidation = false;
+        }
+    
+        if (event == '') {
+          this.phoneValidation = true;
+          this.PhoneMsg = 'Phone Number Input is Empty !';
+        }
+      }
+
+
+      if((res['countryCode'] === '+61')) {
+        this.PhonePatternValid = this.PhonePattern15;
+        if (!this.testPhoneNumber15(event)) {
+          this.phoneValidation = true;
+          this.PhoneMsg = 'Phone Number is Invalid !';
+        }
+    
+        if (this.testPhoneNumber15(event)) {
           this.phoneValidation = false;
         }
     
